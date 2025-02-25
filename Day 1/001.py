ab5ef0e597afd00178369c7301b305653b476032
Roshan Pokhrel <60165267+Roshan-ops@users.noreply.github.com>
2025-02-25 17:55:49 +0545

    new code

--- Day 1/001.py
+++ Day 1/001.py
@@ -1,2 +1,10 @@
-# print("Hello" +  input("What is your name ?"))
-print("hello world")
+# print("Hello" + " "+input("What is your name ?"))
+# print("hello world")
+
+# print(input())
+
+# num1= int(input())
+# num2= int(input())
+# print(num1*num2)
+
+print(len(input("What is your name? ")))
--- Day 1/1.3.py
+++ Day 1/1.3.py
@@ -1,4 +1,14 @@
 # input("what is your name? ")
 # print(len (input("What is your name? ")))
 
- 
+print("Hello world0\nRoshaan Pokhrel")
+
+print("Roshan" + " " + "Pokhrerl")
+
+
+
+
+print("Day 1-Strinf Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello" + "world")')
+print("New lines can be created with a backslash and n.")
