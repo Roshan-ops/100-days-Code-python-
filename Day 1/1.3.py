5269cd5d4031e5aea4ca83fd15d938c05cd281b5
Roshan Pokhrel <60165267+Roshan-ops@users.noreply.github.com>
2025-02-25 16:02:41 +0545

    latest.

--- Day 1/1.3.py
+++ Day 1/1.3.py
@@ -1,8 +1,4 @@
 # input("what is your name? ")
 # print(len (input("What is your name? ")))
 
-print("Mix 500g of Flour, 10g Yeast and #00ml Warm Water in a bowl.")
-print("Knead the dough for 10 minutes.")
-print("Add 3g of Salt.")
-print("Leave the dough to rise for 2hour.")
-print("Bake at 200 degrees for 30 minutes.")
+ 
